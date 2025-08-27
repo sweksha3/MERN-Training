ecc6a7698892d42edb37ec06adef1184ccf57b7d
Sweksha Maurya <149899840+sweksha3@users.noreply.github.com>
2025-08-27 23:07:25 +0530

    Delete day11 directory

--- day11/hello.js
+++
@@ -1,6 +0,0 @@
-console.log("Hello World")
-console.log(3 + 6)
-// console.log(window)      // frontend
-// alert("Good Morning")        // frontend
-// console.log(process)     // backend
-// console.log(global)      // backend
--- day11/read.js
+++
@@ -1,3 +0,0 @@
-const fs=require('fs')
-const data = fs.readFileSync('hi.txt')
-console.log(data)
