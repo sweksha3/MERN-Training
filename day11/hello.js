131fcc109459447c3787907493162cb27e299070
sweksha03 <swekshamaurya2@gmail.com>
2025-08-29 11:07:31 +0530

    Restore day11 first-day files

---
+++ day11/hello.js
@@ -0,0 +1,6 @@
+console.log("Hello World")
+console.log(3 + 6)
+// console.log(window)      // frontend
+// alert("Good Morning")        // frontend
+// console.log(process)     // backend
+// console.log(global)      // backend
